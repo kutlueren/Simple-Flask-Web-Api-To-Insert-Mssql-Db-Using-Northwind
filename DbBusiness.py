631e59954fc1e4afc018efc4c29cca264ade2645
kutlu.eren <kutlu.eren@KEREN>
2018-03-27 09:18:57 +0300

    IOC implementation
    IOC implementation and config file added

--- DbBusiness.py
+++ DbBusiness.py
@@ -1,6 +1,5 @@
 import pyodbc
 
-
 class DbBusiness(object):
 
     def __init__(self, connectionString):
@@ -15,16 +14,20 @@ class DbBusiness(object):
             id = row
 
         cnxn.commit()
+
         cnxn.close()
         return id
 
     def __execute(self, SQLCommand, Values):
         cnxn = pyodbc.connect(self.connectionString)
         cursor = cnxn.cursor()
+
         cursor.execute(SQLCommand, Values)
 
         cnxn.commit()
+
         cnxn.close()
+
 
     def addCategory(self):
         id = self.__getNextId()
@@ -36,23 +39,56 @@ class DbBusiness(object):
 
         return id
 
-    def addCustomer(self, customerId):
-        id = self.__getNextId()
+    def updateCategory(self, categoryId):
+        SQLCommand = "UPDATE Categories SET CategoryName = ?, Description = ? Where CategoryID = ?"
+        Values = [u'testUpdate', u'testUpdate', categoryId]
+        self.__execute(SQLCommand, Values )
 
+    def deleteCategory(self, categoryId):
+        SQLCommand = ("DELETE FROM Categories Where CategoryID = ? ")
+        Values = [categoryId]
+        self.__execute(SQLCommand, Values)
+
+    def addCustomer(self, customerId):
         SQLCommand = ("INSERT INTO Customers VALUES (?,?,?,?,?,?,?,?,?,?,?)")
         Values = [customerId, u'test', u'test', u'test', u'test', u'test', u'test', u'test', u'test', u'test', u'test']
 
         self.__execute(SQLCommand, Values)
 
-        return id
+    def updateCustomer(self, customerId):
+
+        SQLCommand = ("UPDATE Customers SET CompanyName = ?, ContactName = ? WHERE CustomerID = ?")
+        Values = [ u'testUpdate', u'testUpdate', customerId]
+
+        self.__execute(SQLCommand, Values)
+
+    def deleteCustomer(self, customerId):
+
+        SQLCommand = ("DELETE FROM Customers Where CustomerID = ? ")
+        Values = [customerId]
+
+        self.__execute(SQLCommand, Values)
 
     def addRegion(self):
         id = self.__getNextId()
 
-        SQLCommand = ("INSERT INTO Region(RegionID,RegionDescription) VALUES (?,?)")
+        SQLCommand = ("INSERT INTO Region (RegionID,RegionDescription) VALUES (?,?)")
         Values = [id, u'test']
         self.__execute(SQLCommand, Values)
         return id
+
+    def updateRegion(self,regionId):
+
+        SQLCommand = ("UPDATE Region SET RegionDescription = ? WHERE RegionID = ?")
+        Values = [u'testUpdate', regionId]
+
+        self.__execute(SQLCommand, Values)
+
+    def deleteRegion(self,regionId):
+
+        SQLCommand = ("DELETE FROM Region Where RegionID = ? ")
+        Values = [regionId]
+        self.__execute(SQLCommand, Values)
 
     def addSupplier(self):
         id = self.__getNextId()
@@ -62,6 +98,18 @@ class DbBusiness(object):
         self.__execute(SQLCommand, Values)
         return id
 
+    def updateSupplier(self,supplierId):
+        SQLCommand = ("UPDATE Suppliers SET CompanyName = ?, ContactName= ?  WHERE SupplierID = ?")
+        Values = [u'testUpdate',u'testUpdate', supplierId]
+
+        self.__execute(SQLCommand, Values)
+
+    def deleteSupplier(self,supplierId):
+
+        SQLCommand = ("DELETE FROM Suppliers Where SupplierID = ? ")
+        Values = [supplierId]
+        self.__execute(SQLCommand, Values)
+
     def addTerritory(self, regionId):
         id = self.__getNextId()
 
@@ -69,6 +117,18 @@ class DbBusiness(object):
         Values = [str(id), u'test', regionId]
         self.__execute(SQLCommand, Values)
         return id
+
+    def updateTerritory(self, territoryId):
+
+        SQLCommand = ("UPDATE Territories SET TerritoryDescription = ? WHERE TerritoryID = ?")
+        Values = [u'testUpdate',  str(territoryId)]
+        self.__execute(SQLCommand, Values)
+
+    def deleteTerritory(self, territoryId):
+
+        SQLCommand = ("DELETE FROM Territories WHERE TerritoryID = ?")
+        Values = [str(territoryId)]
+        self.__execute(SQLCommand, Values)
 
     def addEmployee(self):
         id = self.__getNextId()
@@ -79,6 +139,18 @@ class DbBusiness(object):
             , u'test', u'test', u'test', 2, u'test']
         self.__execute(SQLCommand, Values)
         return id
+
+    def updateEmployee(self, employeeId):
+
+        SQLCommand = ("UPDATE Employees SET LastName = ?, FirstName = ? WHERE EmployeeID = ?")
+        Values = [u'testUpdate', u'testUpdate',  employeeId]
+        self.__execute(SQLCommand, Values)
+
+    def deleteEmployee(self, employeeId):
+
+        SQLCommand = ("DELETE FROM Employees WHERE EmployeeID = ?")
+        Values = [employeeId]
+        self.__execute(SQLCommand, Values)
 
     def addOrder(self):
         id = self.__getNextId()
@@ -92,5 +164,20 @@ class DbBusiness(object):
         Values = [id, 1,1,1,0]
         self.__execute(SQLCommand, Values)
 
-
         return id
+
+    def updateOrder(self, orderId):
+
+        SQLCommand = ("UPDATE Orders SET ShipName = ?, ShipAddress = ? WHERE OrderID = ?")
+        Values = [u'testUpdate', u'testUpdate',  orderId]
+        self.__execute(SQLCommand, Values)
+
+    def deleteOrder(self, orderId):
+
+        SQLCommand = ("DELETE FROM [Order Details] WHERE OrderID = ?")
+        Values = [orderId]
+        self.__execute(SQLCommand, Values)
+
+        SQLCommand = ("DELETE FROM Orders WHERE OrderID = ?")
+        Values = [orderId]
+        self.__execute(SQLCommand, Values)
--- sql.py
+++ sql.py
@@ -1,29 +1,63 @@
+from configparser import ConfigParser
 from flask import Flask
 from flask import request
 from flask import jsonify
-
+from flask_injector import FlaskInjector
+from injector import singleton, inject
 from DbBusiness import DbBusiness
 
 app = Flask(__name__)
 
-dbBusiness = DbBusiness("Driver={SQL Server Native Client 11.0};"
-                        "Server=yourip;"
-                        "Database=yourdbname;"
-                        "uid=yourusername;pwd=yourpassword")
+
+@app.route('/Index',  methods=['GET'])
+def Index():
+    cfg = ConfigParser()
+    cfg.read("config.ini")
+    str = cfg.get('Settings', 'AppName')
+
+    return str
 
 @app.route('/AddCategory',  methods=['POST'])
-def AddCategory():
+@inject
+def AddCategory(dbBusiness:DbBusiness):
 
     id=dbBusiness.addCategory()
+
 
     result = {
          'Id': id,
     }
 
+    return jsonify({'result': result})
+
+@app.route('/UpdateCategory',  methods=['POST'])
+@inject
+def UpdateCategory(dbBusiness:DbBusiness):
+
+    dbBusiness.updateCategory(request.json['CategoryId'])
+
+
+    result = {
+         'Message': 'Success',
+    }
+
     return jsonify({'result': result}),
 
+@app.route('/DeleteCategory',  methods=['POST'])
+@inject
+def DeleteCategory(dbBusiness:DbBusiness):
+
+    id=dbBusiness.deleteCategory(request.json['CategoryId'])
+
+    result = {
+        'Message': 'Success',
+    }
+
+    return jsonify({'result': result})
+
 @app.route('/AddRegion',  methods=['POST'])
-def AddRegion():
+@inject
+def AddRegion(dbBusiness:DbBusiness):
 
     id = dbBusiness.addRegion()
 
@@ -31,10 +65,72 @@ def AddRegion():
          'Id': id,
     }
 
-    return jsonify({'result': result}),
+    return jsonify({'result': result})
+
+@app.route('/UpdateRegion',  methods=['POST'])
+@inject
+def UpdateRegion(dbBusiness:DbBusiness):
+
+    dbBusiness.updateRegion(request.json['RegionId'])
+
+    result = {
+        'Message': 'Success',
+    }
+
+    return jsonify({'result': result})
+
+
+@app.route('/DeleteRegion',  methods=['POST'])
+@inject
+def DeleteRegion(dbBusiness:DbBusiness):
+
+    dbBusiness.deleteRegion(request.json['RegionId'])
+
+    result = {
+        'Message': 'Success',
+    }
+
+    return jsonify({'result': result})
+
+@app.route('/AddCustomer',  methods=['POST'])
+@inject
+def AddCustomer(dbBusiness:DbBusiness):
+
+    dbBusiness.addCustomer(str(request.json['CustomerId']))
+
+    result = {
+        'Message': 'Success',
+    }
+
+    return jsonify({'result': result})
+
+@app.route('/UpdateCustomer',  methods=['POST'])
+def UpdateCustomer(dbBusiness:DbBusiness):
+
+    dbBusiness.updateCustomer(request.json['CustomerId'])
+
+
+    result = {
+         'Message': 'Success',
+    }
+
+    return jsonify({'result': result})
+
+@app.route('/DeleteCustomer',  methods=['POST'])
+def DeleteCustomer(dbBusiness:DbBusiness):
+
+    dbBusiness.deleteCustomer(request.json['CustomerId'])
+
+
+    result = {
+         'Message': 'Success',
+    }
+
+    return jsonify({'result': result})
 
 @app.route('/AddSupplier',  methods=['POST'])
-def AddSupplier():
+@inject
+def AddSupplier(dbBusiness:DbBusiness):
 
     id = dbBusiness.addSupplier()
 
@@ -44,8 +140,33 @@ def AddSupplier():
 
     return jsonify({'result': result}),
 
+@app.route('/UpdateSupplier',  methods=['POST'])
+@inject
+def UpdateSupplier(dbBusiness:DbBusiness):
+
+    dbBusiness.updateSupplier(request.json['SupplierId'])
+
+    result = {
+        'Message': 'Success',
+    }
+
+    return jsonify({'result': result})
+
+@app.route('/DeleteSupplier',  methods=['POST'])
+@inject
+def DeleteSupplier(dbBusiness:DbBusiness):
+
+    dbBusiness.deleteSupplier(request.json['SupplierId'])
+
+    result = {
+        'Message': 'Success',
+    }
+
+    return jsonify({'result': result})
+
 @app.route('/AddTerritory',  methods=['POST'])
-def AddTerritory():
+@inject
+def AddTerritory(dbBusiness:DbBusiness):
 
     id = dbBusiness.addTerritory(int(request.json['RegionId']))
 
@@ -53,10 +174,35 @@ def AddTerritory():
          'Id': id,
     }
 
-    return jsonify({'result': result}),
+    return jsonify({'result': result})
+
+@app.route('/UpdateTerritory',  methods=['POST'])
+@inject
+def UpdateTerritory(dbBusiness:DbBusiness):
+
+    dbBusiness.updateTerritory(request.json['TerritoryId'])
+
+    result = {
+        'Message': 'Success',
+    }
+
+    return jsonify({'result': result})
+
+@app.route('/DeleteTerritory',  methods=['POST'])
+@inject
+def DeleteTerritory(dbBusiness:DbBusiness):
+
+    dbBusiness.deleteTerritory(request.json['TerritoryId'])
+
+    result = {
+        'Message': 'Success',
+    }
+
+    return jsonify({'result': result})
 
 @app.route('/AddEmployee',  methods=['POST'])
-def AddEmployee():
+@inject
+def AddEmployee(dbBusiness:DbBusiness):
 
     id = dbBusiness.addEmployee()
 
@@ -64,29 +210,78 @@ def AddEmployee():
          'Id': id,
     }
 
-    return jsonify({'result': result}),
+    return jsonify({'result': result})
+
+@app.route('/UpdateEmployee',  methods=['POST'])
+@inject
+def UpdateEmployee(dbBusiness:DbBusiness):
+
+    dbBusiness.updateEmployee(request.json['EmployeeId'])
+
+    result = {
+        'Message': 'Success',
+    }
+
+    return jsonify({'result': result})
+
+@app.route('/DeleteEmployee',  methods=['POST'])
+@inject
+def DeleteEmployee(dbBusiness:DbBusiness):
+
+    dbBusiness.deleteEmployee(request.json['EmployeeId'])
+
+    result = {
+        'Message': 'Success',
+    }
+
+    return jsonify({'result': result})
 
 @app.route('/AddOrder',  methods=['POST'])
-def AddOrder():
+@inject
+def AddOrder(dbBusiness:DbBusiness):
 
     id = dbBusiness.addOrder()
 
-    result = {
-         'Id': id,
-    }
-
-    return jsonify({'result': result}),
-
-@app.route('/AddCustomer',  methods=['POST'])
-def AddCustomer():
-
-    id = dbBusiness.addCustomer(str(request.json['CustomerId']))
 
     result = {
          'Id': id,
     }
 
-    return jsonify({'result': result}),
+    return jsonify({'result': result})
+
+
+@app.route('/UpdateOrder',  methods=['POST'])
+@inject
+def UpdateOrder(dbBusiness:DbBusiness):
+
+    dbBusiness.updateOrder(request.json['OrderId'])
+
+    result = {
+        'Message': 'Success',
+    }
+
+    return jsonify({'result': result})
+
+@app.route('/DeleteOrder',  methods=['POST'])
+@inject
+def DeleteOrder(dbBusiness:DbBusiness):
+
+    dbBusiness.deleteOrder(request.json['OrderId'])
+
+    result = {
+        'Message': 'Success',
+    }
+
+    return jsonify({'result': result})
+
+def configure(binder):
+    cfg=ConfigParser()
+    cfg.read("config.ini")
+    str = cfg.get('Settings','ConnectionString')
+
+    binder.bind(DbBusiness, to= DbBusiness(str), scope=request)
+
+FlaskInjector(app=app, modules=[configure])
 
 if __name__ == "__main__":
     app.run()
